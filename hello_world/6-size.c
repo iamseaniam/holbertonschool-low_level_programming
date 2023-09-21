c5778f1be0ad6719eb8cc3ece78e613acb128a84
iamseaniam <7222@holbertonstudents.com>
2023-09-20 20:06:23 -0700

    Test for Q-6 try 1

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: This program demostrates the use of the printf function.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+
+	return (0);
+}
