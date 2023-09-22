2bb1c4acd4b08d6ba26e32a2ea40d9dbf2187b98
iamseaniam <7222@holbertonstudents.com>
2023-09-22 11:42:40 -0700

    Test for Q-0 try 5

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 /**
 * main - Entry point
 *
@@ -7,11 +8,13 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
 		printf("is positive\n");
 	}
@@ -23,6 +26,5 @@ int main(void)
 	{
 		printf("is negative\n");
 	}
-
 	return (0);
 }
