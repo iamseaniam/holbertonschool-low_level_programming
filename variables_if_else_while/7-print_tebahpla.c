9a1b8d9540c3d87c3ff3152b7d2b6ee3dfa41c38
iamseaniam <7222@holbertonstudents.com>
2023-09-25 09:21:03 -0700

    test for Q-7 try 1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program demonstrates the use of the putchar function print
+ * lowercase English alphabets backwards
+ *
+ * Return:
+ * Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
