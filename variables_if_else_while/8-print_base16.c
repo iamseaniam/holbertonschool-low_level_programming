d99e6c382e4546c8b1eb26da676b02c9dff52237
iamseaniam <7222@holbertonstudents.com>
2023-09-25 10:29:44 -0700

    Etst for Q-8 try 1

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program demonstrates the use of the putchar function to print
+ * lowercase English alphabet without the letters 'q' and 'e.'
+ *
+ * Return:
+ * Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+	char alphabet = 'a';
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+
+	while (alphabet <= 'f')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
