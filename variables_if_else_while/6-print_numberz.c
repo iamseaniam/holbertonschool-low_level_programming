ff574c70dd8a3c11e1b61cb0884ab5013d271bca
iamseaniam <7222@holbertonstudents.com>
2023-09-25 09:11:58 -0700

    Test for Q-6 try 1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program demonstrates the use of the putchar function to print
+ * lowercase English alphabet without the letters 'q' and 'e.'
+ *
+ * Return:
+ * Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
