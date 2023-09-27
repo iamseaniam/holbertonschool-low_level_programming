ac33918256c8313dcd8eda1a393856394ce96634
iamseaniam <7222@holbertonstudents.com>
2023-09-27 09:00:24 -0700

    big booty frogs

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-ufhheifeinh
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z' ) 
+	{
+		return 1;
+	} 
+	else
+	{
+		return 0;
+	}	
+}
+
+#endif
