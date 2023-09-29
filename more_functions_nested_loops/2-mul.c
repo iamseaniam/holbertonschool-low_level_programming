e863d6306f07fa1992933ce3e7e9a4afd7c4b1ce
iamseaniam <7222@holbertonstudents.com>
2023-09-29 14:47:37 -0700

    jkdnfi

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/*
+* _isdigit - Check if a character is a digit (0-9)
+* @c: The character to be checked
+*
+* Description: This fuction checks whether a given character is a digit (0-9).
+*
+* Return: 1 if c is a digit, 0 otherwise.
+*/
+int mul(int a, int b);
+{
+	int result = a * b;
+	printf("Result : %d\n", result);
+	
+	return (0);
+}
