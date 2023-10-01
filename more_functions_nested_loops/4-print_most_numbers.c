f59d7f7520bdc6899c7937b7c0cc6c9fd1759dc8
iamseaniam <7222@holbertonstudents.com>
2023-10-01 11:15:33 -0700

    Test for Q-4 try 1

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		if (digit != '2' && digit != '4')
+		{
+			_putchar(digit);
+		}
+		digit++;
+	}
+
+	_putchar('\n');
+
+}
