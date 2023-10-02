cdcaa17eb20ed6f0a69061c6b91d90e3a06dc904
iamseaniam <7222@holbertonstudents.com>
2023-10-02 11:52:54 -0700

    test for Q-4 try 1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: A pointer to a null-reminated string.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,4 +10,7 @@ int _strlen(char *s);
 void _puts(char *str);
 
 int _putchar(char c);
+
+void print_rev(char *s);
+
 #endif
