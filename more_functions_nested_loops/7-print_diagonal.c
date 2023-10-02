af71527568b3b841d8e5479f6cc7e4ebdf582b24
iamseaniam <7222@holbertonstudents.com>
2023-10-02 09:22:31 -0700

    Test for Q-7 try 3

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_line - Prints a line of underscores in the terminal.
-* @n: The number of underscores to print.
+* print_diagonal - Prints a line of underscores in the terminal.
+* @n: The number of back slashes to print.
 *
 * If n is 0 or less, the function prints an empty line.
 */
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i;
 
