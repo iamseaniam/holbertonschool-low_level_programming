aef466319bd522209184aa342becd5b09a9a7cad
iamseaniam <7222@holbertonstudents.com>
2023-10-02 12:28:14 -0700

    test for q / 7 tyryu 1

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - Prints the second half of a string followed by a new line.
+ * @str: A pointer to a null-terminated string.
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	int start = length / 2;
+
+	if (length % 2 != 0)
+	{
+		start = (length - 1) / 2;
+	}
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
