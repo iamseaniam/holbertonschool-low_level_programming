b3d7aa6f76317d8c8c83fe235e40c1b8dcaa5392
Sean Montgomery <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>
2023-10-10 22:00:23 -0500

    idn

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,11 +5,12 @@ char *_strpbrk(char *s, char *accept)
 	while (*s != '\0')
 	{
 		char *ch = accept;
+
 		while (*ch != '\0')
 		{
 			if (*s == *ch)
 			{
-				return s;
+				return (s);
 			}
 			ch++;
 		}
