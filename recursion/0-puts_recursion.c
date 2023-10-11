64b2ac7e61f469fd6d62969195848f8a68f638f6
Sean Montgomery <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>
2023-10-11 14:10:31 -0500

    diwsn

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * @brief This function prints a string followed by a new line.'
+ *
+ * @param s The string to print.
+ */
 void _puts_recursion(char *s)
 {
 	printf("%s\n", s);
