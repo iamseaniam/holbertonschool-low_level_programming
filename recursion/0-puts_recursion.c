bd5f480e55f4c58ba3ef25f2f0bf7afa0adb87f4
Sean Montgomery <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>
2023-10-11 14:21:57 -0500

    diwsn

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * @brief This function recursively prints a string followed by a new line.
- *
- * This function takes a pointer to a string 's' and recursively prints each 
- * of the string followed by a new line until the end of the string is reached.
- *
- * @param s The string to print.
- */
+*_puts_recursion - This function recursively prints a string followed by a new 
+*@s: The string to print.
+*
+*Description: This function takes a pointer to a string 's' and recursively pri
+*character of the string followed by a new line until the end of the string is 
+*/
 void _puts_recursion(char *s)
 {
 	printf("%s\n", s);
--- recursion/0-puts_recursion.c~
+++ recursion/0-puts_recursion.c~
@@ -1,7 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * @brief This function prints a string followed by a new line.'
+ * @brief This function recursively prints a string followed by a new line.
+ *
+ * This function takes a pointer to a string 's' and recursively prints each 
+ * of the string followed by a new line until the end of the string is reached.
  *
  * @param s The string to print.
  */
