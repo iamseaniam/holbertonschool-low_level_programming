bde5797d76860e52ef88877768a6ef9371125a58
Sean Montgomery <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>
2023-10-10 21:43:35 -0500

    dwin

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,12 +3,13 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int length = 0;
 	int found = 0;
 
-	while (*s && !found) 
+	while (*s && !found)
 	{
 		int accept_index = 0;
+
 		while (accept[accept_index])
 		{
-		if (*s == accept[accept_index]) 
+		if (*s == accept[accept_index])
 		{
 			found = 1;
 			break;
@@ -16,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 		accept_index++;
 	}
 
-	if (found) 
+	if (found)
 	{
 		length++;
 	}
