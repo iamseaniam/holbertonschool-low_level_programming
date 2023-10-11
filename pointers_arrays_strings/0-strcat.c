dc25080f6f9d0e9fc5fdeb61febb9ca9c4704b68
Sean Montgomery <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>
2023-10-10 22:53:23 -0500

    sidn

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = '\0';
+	return (dest);
+}
