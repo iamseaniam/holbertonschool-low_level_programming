b0cea618e4ba828c7159b72f063f647d18f5209f
Sean Montgomery <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>
2023-10-12 10:16:14 -0500

    idsj

--- recursion/0-puts_recursion.c~
+++ recursion/0-puts_recursion.c~
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*_puts_recursion - This function recursively prints a string followed by a new 
+*_puts_recursion - This function recursively prints a string followed by a new
 *@s: The string to print.
 *
 *Description: This function takes a pointer to a string 's' and recursively pri
-*character of the string followed by a new line until the end of the string is 
+*character of the string followed by a new line until the end of the string is
 */
 void _puts_recursion(char *s)
 {
---
+++ recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *factorial - Calculate the factorial of a given number.
+ *@n: The integer for which the factorial is calculated.
+ *
+ *Description: This function takes an integer 'n' and calculates its factorial.
+ *If 'n' is negative, it returns -1 to indicate an error. The factorial of 0 is 1.
+ *
+ *Return: The factorial of 'n', or -1 if 'n' is negative.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	else if (n == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * factorial(n -1);
+	}
+}
