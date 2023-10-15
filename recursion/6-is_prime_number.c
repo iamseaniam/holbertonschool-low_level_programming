92a4bfcc297aa83621d553bc976ed0ea965c9cdd
Sean Montgomery <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>
2023-10-15 13:31:52 -0500

    eiodfdk

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include <math.h>
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+	for (int i = 5; i * i <= n; i += 6)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
