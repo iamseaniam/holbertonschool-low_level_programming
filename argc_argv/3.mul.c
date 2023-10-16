a5c53fec5c2f81b703bebf6a02d1c24fca9c1f33
Sean Montgomery <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>
2023-10-16 14:30:07 -0500

    odj

---
+++ argc_argv/3.mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Description: This function multiplies two numbers provided as command-line a
+ *
+ *  Return: 0 if successful, 1 if there are not enough arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		
+		return (1);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
