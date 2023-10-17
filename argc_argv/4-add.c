8c16b3cc9372c7536bab183306cb3fc36d67a1af
Sean Montgomery <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>
2023-10-16 22:45:57 -0500

    dk

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -17,16 +17,16 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int i, j;
 
-	if (argc == 1) 
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for ( i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		int num = (0);
 
-		for ( j = 0; argv[i][j]; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 			}
 			num = num * 10 + (argv[i][j] - '0');
 		}
-		sum += num ;
+		sum += num;
 	}
 	printf("%d\n", sum);
 
