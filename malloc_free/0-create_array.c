e9783906988772a09715362b71f0edfa07d11ab6
Sean Montgomery <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>
2023-10-18 18:41:14 -0500

    ism

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,9 +1,20 @@
 #include "main.h"
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 	{
-		return (0);
+		return (NULL);
 	}
+	char *array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
 }
