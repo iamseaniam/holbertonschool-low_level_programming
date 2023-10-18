e276d7ad6a77efea4dc07be3646cb9c86a13bf4c
Sean Montgomery <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>
2023-10-18 18:43:05 -0500

    ism

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,11 +3,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = (char *)malloc(size * sizeof(char));
+	*array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 	{
 		return (NULL);
