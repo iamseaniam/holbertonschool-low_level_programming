e5b85f213fa23e6d52dbb6644895c55a5451cf1a
Sean Montgomery <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>
2023-10-23 13:29:30 -0500

    sidn

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,27 +10,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat_str = NULL;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
+	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
-	 if (n >= len2)
-		 concat_len = len1 + len2;
-	 else
-		 concat_len = len1 + n;
+	if (n >= len2)
+	{
+		concat_len = len1 + len2;
+	}
+	else
+	{
+		concat_len = len1 + n;
+	}
+	concat_str = (char *)malloc((concat_len + 1) * sizeof(char));
+	if (concat_str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(concat_str, s1);
+	strncat(concat_str, s2, n);
+	concat_str[concat_len] = '\0';
 
-	 concat_str = (char *)malloc((concat_len + 1) * sizeof(char));
-	 if (concat_str == NULL)
-		 return NULL;
-
-	 strcpy(concat_str, s1);
-
-	 strncat(concat_str, s2, n);
-
-	 concat_str[concat_len] = '\0';
-
-	 return concat_str;
+	return (concat_str);
 }
