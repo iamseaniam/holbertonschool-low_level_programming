91e951b96bbff6ef2f7142b80975d5aa2bd1d9b2
Sean Montgomery <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>
2023-10-23 12:56:57 -0500

    sidhni

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,13 +1,1 @@
-#include <stdio.h>
-
 #define ABS(x) ((x) < 0 ? -(x) : (x))
-
-int main(void)
-{
-	int num = -5;
-	int absNum = ABS(num);
-
-	printf("Absolute value of %d is %d\n", num, absNum);
-
-	return (0);
-}
---
+++ preprocessor/3-function_like_macro.h~
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+int main(void)
+{
+	int num = -5;
+	int absNum = ABS(num);
+
+	printf("Absolute value of %d is %d\n", num, absNum);
+
+	return (0);
+}
