5b85fe1cfeafce1262aa195003f598dca1616d43
Sean Montgomery <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>
2023-10-23 12:40:44 -0500

    sidhni

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: 0 on success
+ *     */
 int main(void)
 {
 	printf("Compiled from file: %s\n", __FILE__);
+
 	return (0);
 }
+
