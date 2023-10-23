6fb0c67cbbf964ed35c4e090b0aafe75870989b7
Sean Montgomery <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>
2023-10-23 13:56:15 -0500

    sidn

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+
+int *array_range(int min, int max)
+{
+	int size;
+	int *arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = max - min + 1;
+	arr = (int *)malloc(size * sizeof(int));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
+
