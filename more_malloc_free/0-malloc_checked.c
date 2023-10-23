b6672e5325bcbf6279d1a3acd076bfcd04d427c5
Sean Montgomery <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>
2023-10-23 14:03:53 -0500

    sidn

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "malloc failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
