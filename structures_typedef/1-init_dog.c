38f97327421420d02a286844874dfa9df16aa103
Sean Montgomery <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>
2023-10-25 12:21:22 -0500

    fixed stuff

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,12 @@
 #include "dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("Ok");
+		return (void);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
