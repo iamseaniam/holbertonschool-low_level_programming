42344b779b4b0feece025885de9cb2c16420f961
Sean Montgomery <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>
2023-10-26 13:11:51 -0500

    somf

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
-
+/**
+ *new_dog - Creates a new dog structure.
+ *@name: The name of the dog.
+ *@age: The age of the dog.
+ *@owner: The owner of the dog.
+ *
+ *This function allocates memory for a new dog structure and initializes
+ *its fields with the provided name, age, and owner values. It returns a
+ *pointer to the newly created dog structure, or NULL if any memory
+ *allocation fails.
+ *
+*Return: A pointer to the new dog structure, or NULL if memory allocation fails
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 	{
 		return (NULL);
