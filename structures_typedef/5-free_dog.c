da47543656bffe0be973d0af912979f2b56c504e
Sean Montgomery <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>
2023-10-26 21:55:24 -0500

    somf

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ *free_dog - Frees memory allocated for a dog structure
+ *@d: Pointer to the dog structure to be freed
+ *
+ *This function frees the memory allocated for a dog structure. If the pointer
+ *'d' is NULL, it does nothing.
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
