9966d42d77e0c727ea5edf94810000ae3f80b32a
Sean Montgomery <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>
2023-10-27 23:44:20 -0500

    oadej

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,24 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A simple C program that demonstrates the use of function pointers
+* to print a name to the console.
+*
+*Return: 0 (Success)
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
+}
+void print_to_console(char *name)
+{
+	printf("Name: %s\n", name);
+}
+int main()
+{
+	char name[] = "John Doe";
+	print_name(name, print_to_console);
+	return (0);
 }
