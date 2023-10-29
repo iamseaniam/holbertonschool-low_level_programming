1d376e6d2f0509e232806e5467ca5f4a46aa55b5
Sean Montgomery <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>
2023-10-29 13:05:16 -0500

    sidm

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,6 +4,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (i == NULL || element == NULL)
+	{
+		return;
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
@@ -14,14 +19,3 @@ void print_element(int element)
 {
 	printf("%d ", element);
 }
-/**
-*int main()
-*{
-*	int array[] = {1, 2, 3, 4, 5};
-*	size_t size = sizeof(array) / sizeof(array[0]);
-*
-*	array_iterator(array, size, print_element);
-*
-*	return (0);
-*}
-*/
