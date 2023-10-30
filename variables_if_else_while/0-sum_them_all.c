4a86ecc0b72d1d8fe09c00c63831f269afa6ca28
Sean Montgomery <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>
2023-10-30 12:29:08 -0500

    dkn

---
+++ variables_if_else_while/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	int sum = 0;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+
+	return (sum);
+}
