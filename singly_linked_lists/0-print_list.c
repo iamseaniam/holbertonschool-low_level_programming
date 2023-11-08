7b07d906d848660329cdb61861aaf785b7e26eb4
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 14:16:51 -0600

    moved if loop after while loop

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,16 +5,16 @@ size_t print_list(const list_t *h)
 	size_t size = 0;
 	const list_t *node = h;
 
-	if (node->str == NULL)
-	{
-		printf("[0], (nil)");
-	}
-
 	while (node != NULL)
 	{
 		printf("%s\n", node->str);
 		node = node->next;
 		size++;
+	}
+
+	if (node->str == NULL)
+	{
+		printf("[0], (nil)");
 	}
 	return (size);
 }
