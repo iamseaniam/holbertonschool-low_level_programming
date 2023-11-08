85d407b5c56ee4b2cd5c75f99262e7add3cfe752
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 14:10:39 -0600

    checking if node str is null

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,6 +5,11 @@ size_t print_list(const list_t *h)
 	size_t size = 0;
 	const list_t *node = h;
 
+	if (node->str == NULL)
+	{
+		printf("[0], (nil)");
+	}
+
 	while (node != NULL)
 	{
 		printf("%s\n", node->str);
@@ -12,5 +17,4 @@ size_t print_list(const list_t *h)
 		size++;
 	}
 	return (size);
-}	
-
+}
