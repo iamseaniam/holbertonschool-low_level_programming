5fe19dc2575c050dd746a7784798983f6ec93be4
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 13:59:18 -0600

    change tabs so there are no spaces

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,15 +2,15 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t size = 0;
-    const list_t *node = h;
+	size_t size = 0;
+	const list_t *node = h;
 
-    while (node != NULL)
-    {
-        printf("%s\n", node->str);
-        node = node->next;
-        size++;
-    }
-    return (size);
-}
+	while (node != NULL)
+	{
+		printf("%s\n", node->str);
+		node = node->next;
+		size++;
+	}
+	return (size);
+}	
 
