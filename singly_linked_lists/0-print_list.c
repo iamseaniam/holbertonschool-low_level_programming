16424080959f759de205d38c2a2263b47168fc71
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 13:28:38 -0600

    made file 0 and main.h

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+size_t print_list(const list_t *h);
+{
+    if (*str == NULL)
+        print("[0] (nil)");
+
+    while node:
+    {
+        printf(node.value)
+        node = node.next
+    }
+    return(h)
+}
+
