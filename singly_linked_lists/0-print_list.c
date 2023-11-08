deabfcfff987f32de5cda274dd003aa5071c1393
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 14:53:51 -0600

    added betty commet

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the start of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_list(const list_t *h)
 {
 	size_t size = 0;
@@ -10,7 +16,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", (int)strlen(node->str), node->str);
-		
+
 		node = node->next;
 		size++;
 	}
