39e88ecf2f0a5c74548a3974b7ef1601b2fdc59a
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 13:44:21 -0600

    fixed errors

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,12 +5,14 @@ size_t print_list(const list_t *h)
     char *str;
     const list_t *node = h;
 
-    if (*str == NULL)
+    if (str == NULL)
+    {
         printf("[0] (nil)");
-
+    }
+    
     if (node != NULL)
     {
-        printf("%s\n"), node->str);
+        printf("%s\n", node->str);
         node = node->next;
     }
 }
