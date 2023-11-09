fe4460931bd330f6eddb3b3eb727f801a659967e
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 18:58:44 -0600

    removed printf

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,11 +7,6 @@ size_t list_len(const list_t *h)
 
 	while (node != NULL)
 	{
-		if (node->str == NULL)
-			return (size);
-		else
-			printf("%s\n", node->str);
-
 		node = node->next;
 		size++;
 	}
