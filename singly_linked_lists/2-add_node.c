57af022031815ba11f2254b7b0802cad6ed4e0dc
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-08 19:56:25 -0600

    added malloc and dupped new str with strdup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,10 +2,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node = NULL;
+
 	if (*head == NULL)
 	{
 		list_t *new_node = malloc(sizeof(list_t));
 		if (new_node == NULL)
 			return (NULL);
 	}
+
+	char *clonedSheep_str = strdup(str);
+	if (clonedSheep_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 }
