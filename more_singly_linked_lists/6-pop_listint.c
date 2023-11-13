80f5345db3dfa840e0a468479fad24e6ffe1f6b8
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-13 12:30:05 -0600

    Saturdays work, errors and main files gcc in term

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of a listint_t linked list, and returns
+ * the head node's data (n).
+ * @head: Double pointer to the head of the list.
+ *
+ * Return: If the linked list is empty - 0. Otherwise - the head node's data (n).
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *old_head;
+	int n;
+
+	if (*head == NULL)
+	{
+     	return 0;
+	}
+
+	old_head = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+	free(old_head);
+
+	return n;
+}
