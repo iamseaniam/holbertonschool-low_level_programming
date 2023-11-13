f1ee29d05f4e5146887a6cf5fcf2b63bcc39efc1
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-13 12:48:33 -0600

    EROORS

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at index of a listint_t linked list.
+ * delete_nodeint_at_index - Deletes the node at index of a listint_t linked li
  * @head: Double pointer to the head of the list.
  * @index: The index of the node that should be deleted. Index starts at 0.
  *
@@ -15,19 +15,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	temp = *head;
 
 	if (index == 0)
 	{
-     	*head = temp->next;
-     	free(temp);
-     	return 1;
+		*head = temp->next;
+		free(temp);
+		return (1);
 	}
 
-    for (i = 0; temp != NULL && i < index - 1; i++)
+	for (i = 0; temp != NULL && i < index - 1; i++)
 	{
 		temp = temp->next;
 	}
--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,11 +3,11 @@
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position.
  * @head: Double pointer to the head of the list.
- * @idx: The index of the list where the new node should be added. Index starts at 0.
+ * @idx: The index of the list where the new node should be added. Index starts
  * @n: The integer to be included in the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
- *         If it is not possible to add the new node at index idx, do not add the new node and return NULL.
+ *If it is not possible to add the new node at index idx, do not add the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->n = n;
