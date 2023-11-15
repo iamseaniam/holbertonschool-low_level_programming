06b18e1b198e4b5cf803ce2be67276c37e677d78
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-15 12:03:56 -0600

    betty comment and newline

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
+ * dlistint_len - Prints all the elements of a dlistint_t list.
  * @h: Pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
