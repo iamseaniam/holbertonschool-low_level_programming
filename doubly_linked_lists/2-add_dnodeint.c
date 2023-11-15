8800e0d401a6104e0c8fc8dbcc3df56973489b17
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-15 12:38:51 -0600

    if statement and betty comment

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,8 +1,17 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: Double pointer to the head of the dlistint_t list.
+ * @n: The integer value of the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
 
 	newNode->n = n;
 	newNode->next = *head;
@@ -12,7 +21,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = newNode;
 
 	(*head) = newNode;
-
 
 	return (newNode);
 
