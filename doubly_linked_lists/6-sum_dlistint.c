c12c6dd72eeaffd694de525858eb585c03bdb4e4
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-16 12:27:21 -0600

    kernel errors fixed

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint - a function that returns the sum of all the data (n) of a
+ * dlistint_t linked list.
+ * @head: pointer to the head of the list
+ *
+ * Return: sum of all the data (n) of a dlistint_t linked list.
+ * If the list is empty, return 0.
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *ptr = head;
@@ -10,7 +17,7 @@ int sum_dlistint(dlistint_t *head)
 
 	while (ptr != NULL)
 	{
-		sum += ptr-> n;
+		sum += ptr->n;
 		ptr = ptr->next;
 	}
 	return (sum);
