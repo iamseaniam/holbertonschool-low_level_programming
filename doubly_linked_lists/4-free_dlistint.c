36167c0e2fbcc383bdb0c1c8b2c718321b9073ab
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-15 19:22:11 -0600

    file 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - Frees a list_t list.
+ * @head A pointer to the hed of the list_t list.
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
