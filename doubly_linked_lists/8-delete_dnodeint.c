5e3bbd6831b5bc0d8ec670cc73f2c572bf749dc8
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-16 19:32:36 -0600

    trailing whitespace

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked 
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked
  * @head: double pointer to the head of the list
  * @index: index of the node that should be deleted, index starts at 0
  * Return: 1 if it succeeded, -1 if it failed
