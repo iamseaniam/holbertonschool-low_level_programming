656b53c638410534e32f803a400b1ff0b6918c9b
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-17 23:22:50 -0600

    aaaa

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,14 +1,30 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ * If something went wrong, returns NULL
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	struct hash_table_t = (struct hashtable*)malloc(sizeof(struct hashtable));
-	hashtable->size = size;
-	hashtable->table = (struct Entry**)malloc(sizeof(struct entry*));
+	hash_table_t *new_hash_table;
+	unsigned long int i;
 
-	for (int i = 0; i < size; ++i)
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+     	return NULL;
+
+	new_hash_table->size = size;
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash_table->array == NULL)
 	{
-		hashtable->table[i] = NULL;
+		free(new_hash_table);
+		return NULL;
 	}
-	return hashtable;
+
+	for (i = 0; i < size; i++)
+		new_hash_table->array[i] = NULL;
+
+	return new_hash_table;
 }
