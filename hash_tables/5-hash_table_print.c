009a0a6632aa2e58b6e3e6c813a6f80593730fc4
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-20 23:00:11 -0600

    file 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table
+ *
+ * This function prints each key/value pair in the hash table in the order
+ * that they appear in the array of the hash table. If the hash table is NULL,
+ * it doesn't print anything.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char *sep = "";
+	
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
