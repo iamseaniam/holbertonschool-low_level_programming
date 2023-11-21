f7b3d1b5142ca9bc3638e1aa315ed2634f4ca1a7
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-20 22:03:01 -0600

    file 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *current_node;
 
 	if (!ht || !key || !*key || !value)
-     	return (0);
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
@@ -26,16 +26,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(current_node->value);
 			current_node->value = strdup(value);
-				if (!current_node->value)
+			if (!current_node->value)
 				return (0);
-				return (1);
+			return (1);
 		}
-     	current_node = current_node->next;
+		current_node = current_node->next;
 	}
 
-	new_node = create_new_node(key, value);
+	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
+
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+		}
+
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
