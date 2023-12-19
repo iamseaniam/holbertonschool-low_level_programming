45409cd2077a4f9d2fa92be8e3f8eb6fac7ab66c
iamseaniam <7222@holbertonstudents.com>
2023-12-18 22:33:54 -0600

    .H errors

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,6 +5,6 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);=
+int binary_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
-#endif**
+#endif
